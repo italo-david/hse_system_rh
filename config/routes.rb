8ecab3bc0724525a08f019ab9d27ba1a1f36e9ca
Italo David <italo_joga10@hotmail.es>
2021-01-13 16:31:30 -0300

    Atualizando

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
   
   resources :colaborattors, except: [:show]
 
